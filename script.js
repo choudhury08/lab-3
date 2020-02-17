b0c7e299a4d59a0cdaf8b8c576a1dd0bb5c7ca46
Mahbubur Choudhury <mahbubc2012@gmail.com>
2020-02-17 17:23:57 -0500

    git remote add origin https://github.com/choudhury08/lab-3.git git push -u origin master

---
+++ script.js
@@ -0,0 +1,24 @@
+const game = prompt(
+  "Would you like to play a game with two character yes or no?"
+);
+const name = prompt("");
+if (game === no) {
+  console.log("Thank you for playing");
+} else game;
+
+if (damageAmount == 0) {
+  damage = 0;
+  playerHealth = playerHealth - damage;
+  System.out.println(playerHealth);
+} else if (damageAmount == 1) {
+  damage = 1;
+  playerHealth = playerHealth = damage;
+  System.out.println(playerHealth);
+} else if (damageAmount == 2) {
+  damage = 2;
+  playerHealth = playerHealth - damage;
+  System.out.println(playerHealth);
+}
+if ((playerHealth = 0)) {
+  gameover();
+}
